955cb2fec88aaea9904c4c28abe8139cf622ba63
Ryan Boudwin <rboudwin@student.hive.fi>
2024-08-12 16:35:14 +0300

    basic functionality achieved for ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 14:10:49 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/08/12 15:56:21 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/08/12 16:34:46 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,26 @@ int main(int argc, char **argv)
 	
 	std::string filename = argv[1];
 	std::string s1 = argv[2];
-	std::string s2 = argv[3];
-	std::string out_file = filename;
-	out_file.append(".replace");
+	std::string out_filename = filename;
+	out_filename.append(".replace");
 
 	std::ifstream in_file(filename);
-	std::ofstream file(out_file);
+	std::ofstream out_file(out_filename);
 	
 	for (std::string line; std::getline(in_file, line);)
 	{
-		if line.find(s1) != std::npos;
+		size_t suspect;
+		suspect = line.find(s1);
+		while (suspect != std::string::npos)
+		{
+			line.erase(suspect, s1.length());
+			line.insert(suspect, argv[3]);
+			suspect = line.find(s1);
+		}
+		out_file << line << std::endl;
 	}
-	std::cout << out_file << filename;
-	file << "Your momma says" << " all kinds of stuff" << std::endl;
-	file.close();
+	out_file.close();
+	in_file.close();
+
+	// still needs to handle bad files and bad permissions. 
 }
