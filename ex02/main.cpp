52c77355c2bfe4b3fa79ab2bc49e0c90f4a856ee
Ryan Boudwin <rboudwin@student.hive.fi>
2024-08-14 12:33:45 +0300

    now printing both the address of stringPTR and what it is pointing to

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 17:10:14 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/08/01 17:15:28 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/08/14 12:32:35 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int main(void)
     std::string &stringREF = str;
     
     std::cout << "The memory address of str is " << &str << std::endl;
-    std::cout << "The memory address of stringPTR is " << &stringPTR << std::endl;
+    std::cout << "The memory address of stringPTR is " << &stringPTR << " and it points to " << stringPTR << std::endl;
     std::cout << "The memory address of stringREF is " << &stringREF << std::endl;
     std::cout << "The value of str is " << str << std::endl;
     std::cout << "The value pointed to by stringPTR is " << *stringPTR << std::endl;
